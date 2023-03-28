a22c4a0cc14c849625621a9fbf781b08cdcb415d
Osiyomeoh <samuelaleonomoh5@gmail.com>
2023-03-28 08:20:09 +0100

    f

---
+++ client/build/precache-manifest.7701a98b7a32c2485897f88c04b67693.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae83085b1c3625dde635",
+    "url": "/static/css/main.72002f6d.chunk.css"
+  },
+  {
+    "revision": "ae83085b1c3625dde635",
+    "url": "/static/js/main.ae83085b.chunk.js"
+  },
+  {
+    "revision": "266f41707c8a4695be37",
+    "url": "/static/css/1.4273bb59.chunk.css"
+  },
+  {
+    "revision": "266f41707c8a4695be37",
+    "url": "/static/js/1.266f4170.chunk.js"
+  },
+  {
+    "revision": "37297dd22fc50943dc04",
+    "url": "/static/js/2.37297dd2.chunk.js"
+  },
+  {
+    "revision": "e9397265cdef6a750c15",
+    "url": "/static/js/runtime~main.e9397265.js"
+  },
+  {
+    "revision": "9cff63aa16902bedd6f27978696f0b38",
+    "url": "/static/media/neww.9cff63aa.png"
+  },
+  {
+    "revision": "657e054a1f76eee288152b0a4df4220e",
+    "url": "/static/media/sym.657e054a.png"
+  },
+  {
+    "revision": "c2ec8ddde44fae32c37f10fde0d6fb34",
+    "url": "/static/media/new_cool.c2ec8ddd.jpg"
+  },
+  {
+    "revision": "7ef17452045ecbc3db31b70cd9e4d61b",
+    "url": "/index.html"
+  }
+];
